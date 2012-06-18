52ca3edaf52eb26160e92b8aa8586f0cb6b2cdaa
Toshiaki Yamane <yamanetoshi@gmail.com>
2012-06-18 15:17:10 +0900

    initial commit

---
+++ src/jp/shuri/yamanetoshi/cvt/CalendarViewTestActivity.java
@@ -0,0 +1,13 @@
+package jp.shuri.yamanetoshi.cvt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalendarViewTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
